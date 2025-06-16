edab375543227d8d79140c68363a3a51c4c9859b
Kishore-MK <tamiltac6@gmail.com>
2025-06-17 03:38:34 +0530

    boilerplate setup

---
+++ tgbot/src/index.ts
@@ -0,0 +1,9 @@
+import {Bot} from "grammy"
+// Create a bot object
+const bot = new Bot(""); // <-- place your bot token in this string
+
+// Register listeners to handle messages
+bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
+
+// Start the bot (using long polling)
+bot.start();
